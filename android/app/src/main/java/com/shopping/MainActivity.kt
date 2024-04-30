dcbd3e078b427888bac1def5a7c4097196944173
Vishwesh Dhoble <vishweshdhoble0610@gmail.com>
2024-05-01 02:29:26 +0530

    shopping minor project

--- android/app/src/main/java/com/shopping/MainActivity.kt
+++ android/app/src/main/java/com/shopping/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.shopping
 
 import com.facebook.react.ReactActivity
+import android.os.Bundle;
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
@@ -12,11 +13,15 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "shopping"
-
+  
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 }
